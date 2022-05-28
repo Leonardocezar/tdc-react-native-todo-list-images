cdf83901fcb92be65554bc1f05ab6fa48788c61c
Leonardo Cezar <leonardo.cezar@olist.com>
2022-05-28 16:38:11 -0300

    add icons and init project

---
+++ Assets.ts
@@ -0,0 +1,20 @@
+class Assets {
+  get bookIcon() {
+    return require("./icons/book.png");
+  }
+  get folderIcon() {
+    return require("./icons/folder.png");
+  }
+  get highlitherIcon() {
+    return require("./icons/highlither.png");
+  }
+  get mortalboardIcon() {
+    return require("./icons/mortalboard.png");
+  }
+  get paperplaneIcon() {
+    return require("./icons/paper-plane.png");
+  }
+  get telescopeIcon() {
+    return require("./icons/telescope.png");
+  }
+}
